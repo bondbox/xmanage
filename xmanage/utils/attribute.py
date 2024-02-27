35090354c06e61bfc053f3270f83e22de7b3de2d
Mingzhe Zou <zoumingzhe@qq.com>
2024-02-27 17:19:33 +0800

    setup: attribute: fix __url_bugs__ error

--- xmanage/utils/attribute.py
+++ xmanage/utils/attribute.py
@@ -5,7 +5,7 @@ from urllib.parse import urljoin
 __name__ = "xmanage"
 __version__ = "0.3"
 __description__ = "System and service management tool."
-__url_home__ = "https://github.com/bondbox/xmanage"
+__url_home__ = "https://github.com/bondbox/xmanage/"
 __url_code__ = __url_home__
 __url_docs__ = __url_home__
 __url_bugs__ = urljoin(__url_home__, "issues")
