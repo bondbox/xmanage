57f10e4026923e0e5a34be5c5b336ff3d9365b02
Mingzhe Zou <zoumingzhe@qq.com>
2025-03-26 09:18:09 +0800

    ci: fixup pylint W0718(broad-exception-caught)

--- xmanage/__init__.py
+++ xmanage/__init__.py
@@ -4,5 +4,5 @@ from .utils import systemd_service  # noqa:F401
 
 try:
     from .utils import systemd_path  # noqa:F401
-except Exception:
+except Exception:  # pylint: disable=broad-exception-caught
     pass
